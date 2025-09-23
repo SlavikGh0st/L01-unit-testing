2f1ee71067e641ea0956e12f4e729d61a2ea8396
antvu <antvu@skbkontur.ru>
2025-09-23 17:35:28 +0500

    Добавил CeasarCryptor c тестами

---
+++ L01-unit-testing/CeasarCryptor.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace L01_unit_testing;
+
+public class CeasarCryptor
+{
+    private const int DefaultShift = 3;
+    private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public string Encrypt(string text) => text
+        .Select(GetEncryptedChar)
+        .Aggregate(new StringBuilder(), (builder, @char) => builder.Append(@char))
+        .ToString();
+
+    public string Decrypt(string text) => text
+        .Select(GetDecryptedChar)
+        .Aggregate(new StringBuilder(), (builder, @char) => builder.Append(@char))
+        .ToString();
+
+    private char GetEncryptedChar(char @char)
+    {
+        var index = Alphabet.IndexOf(@char, StringComparison.OrdinalIgnoreCase);
+        var encryptedChar = Alphabet[index + DefaultShift];
+        return char.IsLower(@char) ? char.ToLower(encryptedChar) : char.ToUpper(encryptedChar);
+    }
+
+    private char GetDecryptedChar(char @char)
+    {
+        var index = Alphabet.IndexOf(@char, StringComparison.OrdinalIgnoreCase);
+        var encryptedChar = Alphabet[index - DefaultShift];
+        return char.IsLower(@char) ? char.ToLower(encryptedChar) : char.ToUpper(encryptedChar);
+    }
+}
---
+++ L01-unit-testing/CeasarCryptorTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+namespace L01_unit_testing;
+
+public class CeasarCryptorTests
+{
+    [Test]
+    public void Encrypt_LowerCase()
+    {
+        const string text = "abc";
+        const string expected = "def";
+        var encryptor = new CeasarCryptor();
+
+        var actual = encryptor.Encrypt(text);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Encrypt_UpperCase()
+    {
+        const string text = "ABC";
+        const string expected = "DEF";
+        var encryptor = new CeasarCryptor();
+
+        var actual = encryptor.Encrypt(text);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Decrypt_LowerCase()
+    {
+        const string text = "def";
+        const string expected = "abc";
+        var encryptor = new CeasarCryptor();
+
+        var actual = encryptor.Decrypt(text);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Decrypt_UpperCase()
+    {
+        const string text = "DEF";
+        const string expected = "ABC";
+        var encryptor = new CeasarCryptor();
+
+        var actual = encryptor.Decrypt(text);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
