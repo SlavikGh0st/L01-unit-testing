73ab73b58f2dfb0efad0e550e89bba1ed00973b7
antvu <antvu@skbkontur.ru>
2025-10-14 17:39:38 +0500

    Рефакторинг тестов

--- L01-unit-testing/CeasarCryptorTests.cs
+++ L01-unit-testing/CeasarCryptorTests.cs
@@ -4,50 +4,24 @@ namespace L01_unit_testing;
 
 public class CeasarCryptorTests
 {
-    [Test]
-    public void Encrypt_LowerCase()
+    [TestCase("abc", "def")]
+    [TestCase("XYZ", "ABC")]
+    public void Encrypt_WorksCorrectly(string input, string expected)
     {
-        const string text = "abc";
-        const string expected = "def";
         var encryptor = new CeasarCryptor();
 
-        var actual = encryptor.Encrypt(text);
+        var actual = encryptor.Encrypt(input);
 
         Assert.That(actual, Is.EqualTo(expected));
     }
 
-    [Test]
-    public void Encrypt_UpperCase()
+    [TestCase("def", "abc")]
+    [TestCase("ABC", "XYZ")]
+    public void Decrypt_WorksCorrectly(string input, string expected)
     {
-        const string text = "ABC";
-        const string expected = "DEF";
         var encryptor = new CeasarCryptor();
 
-        var actual = encryptor.Encrypt(text);
-
-        Assert.That(actual, Is.EqualTo(expected));
-    }
-
-    [Test]
-    public void Decrypt_LowerCase()
-    {
-        const string text = "def";
-        const string expected = "abc";
-        var encryptor = new CeasarCryptor();
-
-        var actual = encryptor.Decrypt(text);
-
-        Assert.That(actual, Is.EqualTo(expected));
-    }
-
-    [Test]
-    public void Decrypt_UpperCase()
-    {
-        const string text = "DEF";
-        const string expected = "ABC";
-        var encryptor = new CeasarCryptor();
-
-        var actual = encryptor.Decrypt(text);
+        var actual = encryptor.Decrypt(input);
 
         Assert.That(actual, Is.EqualTo(expected));
     }
