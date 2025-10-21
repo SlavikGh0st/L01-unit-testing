972bbe1e9c14085e35e817302d3fe896391bc3d0
antvu <antvu@skbkontur.ru>
2025-10-21 15:48:26 +0500

    Сделал задание на сериализацию

---
+++ L02-serialization/Printer.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace L02_serialization;
+
+public static class Printer
+{
+    private static readonly HashSet<Type> SimpleTypes = new()
+    {
+        typeof(string),
+        typeof(int),
+    };
+
+    public static string PrintToString<T>(T? obj)
+    {
+        if (obj == null)
+            return "null" + Environment.NewLine;
+
+        var type = obj.GetType();
+        var stringBuilder = new StringBuilder(type.Name).AppendLine();
+        
+        var properties = type.GetProperties();
+        foreach (var prop in properties)
+        {
+            stringBuilder.Append($"\t{prop.Name} = {prop.GetValue(obj)}").AppendLine();
+        }
+
+        return stringBuilder.ToString();
+    }
+}
---
+++ L02-serialization/PrinterTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using L02_serialization.Models;
+using NUnit.Framework;
+
+namespace L02_serialization;
+
+public class PrinterTests
+{
+    [Test]
+    public void PrintToString_PrintPerson()
+    {
+        var person = new Person("Slava", 33);
+
+        var actual = Printer.PrintToString(person);
+
+        Console.WriteLine(actual);
+        actual.Should().Be("Person\r\n\tName = Slava\r\n\tAge = 33\r\n");
+    }
+    
+    [Test]
+    public void PrintToString_PrintPosition()
+    {
+        var person = new Person("Slava", 33);
+        var pos = new Position("Преподаватель ООП", new DateTime(2025, 9, 1), person);
+
+        var actual = Printer.PrintToString(pos);
+
+        Console.WriteLine(actual);
+        actual.Should().Be("Position\r\n\tName = Преподаватель ООП\r\n\tStartAt = 01.09.2025\r\n\tPerson = Person\r\n\t\tName = Slava\r\n\t\tAge = 33\r\n");
+    }
+}
